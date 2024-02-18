3d08d98850f11e766f351cfd215c3f1479f278c2
diiz2445 <ax90790@gmail.com>
2024-02-18 17:55:51 +0500

    2 commit(get names, codes nope)

--- parser selenium/Class for parse names.cs
+++ parser selenium/Class for parse names.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using System;
 using System.Collections.Generic;
@@ -8,13 +9,31 @@ using System.Threading.Tasks;
 
 namespace parser_selenium
 {
+
     internal class Class_for_parse_names
     {
         string info;
-        Dictionary<string,int> names;
-        IWebDriver driver = new EdgeDriver();
+        Dictionary<string, int> names;
 
+        public Class_for_parse_names()
+        {
+            names = new Dictionary<string, int>();
+        }
 
+        public static void b()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Minimize();
+            driver.Url = "https://buff.163.com/market/csgo";
+            List<IWebElement> elements = driver.FindElements(By.XPath("//*[@id=\"j_list_card\"]/ul/li/h3/a")).ToList();
+            List<IWebElement> codes = driver.FindElements(By.XPath("//*[@id=\"j_list_card\"]/ul/li/h3")).ToList();
 
+            foreach (IWebElement element in codes)
+            {
+                Console.WriteLine(element.Text);
+                Console.WriteLine(element.GetAttribute("href"));
+            }
+        }
+        
     }
 }
