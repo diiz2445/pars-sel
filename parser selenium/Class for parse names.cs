6bebd2df6936d6c36dd883be34b571d44c302f8d
diiz2445 <ax90790@gmail.com>
2024-02-18 14:50:17 +0500

    first commit

---
+++ parser selenium/Class for parse names.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parser_selenium
+{
+    internal class Class_for_parse_names
+    {
+        string info;
+        Dictionary<string,int> names;
+        IWebDriver driver = new EdgeDriver();
+
+
+
+    }
+}
