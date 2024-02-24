4b83bff30005d9cf53cf32dbdc60b972d2f795f7
Tara Routray <ax90790@gmail.com>
2024-02-24 12:03:32 +0500

    add serialization

--- parser selenium/Class for parse names.cs
+++ parser selenium/Class for parse names.cs
@@ -6,14 +6,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace parser_selenium
 {
-
+    [Serializable]
     internal class Class_for_parse_names
     {
-        string info;
-        Dictionary<string, int> names;
+        
+        public Dictionary<string, int> names;
 
         public Class_for_parse_names()
         {
@@ -26,7 +29,8 @@ namespace parser_selenium
             driver.Manage().Window.Minimize();
             driver.Url = "https://buff.163.com/market/csgo";
             List<IWebElement> elements = driver.FindElements(By.XPath("//*[@id=\"j_list_card\"]/ul/li/h3/a")).ToList();
-            
+            try { names = deserialize(); Console.WriteLine("начальная сер пройдено"); }
+            catch { }
             foreach (IWebElement element in elements)
             {
                 Console.WriteLine(element.Text);
@@ -38,13 +42,36 @@ namespace parser_selenium
                     try
                     {
                         names.Add(element.Text, int.Parse(str));
+                        Console.WriteLine("добавлен");
                     }
-                    catch
-                    { }
+                    catch(Exception e)
+                    { Console.WriteLine(e.Message); }
                 }
+                
+            }
+            serialize();
+        }
+        public void serialize()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("codes.txt", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, names);
 
+                Console.WriteLine("Объект сериализован");
             }
         }
-        
+        public static Dictionary<string,int> deserialize()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("codes.txt", FileMode.Open))
+            {
+                Dictionary<string, int> newNames = (Dictionary<string, int>)formatter.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Info: {newNames}");
+                return newNames;
+            }
+        }
     }
 }
