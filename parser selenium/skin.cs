44634f1a45ec7a12fb9c2d3390956cd9dd809894
Alex <ax90790@gmail.com>
2024-08-17 20:56:17 +0500

    переписал с сохранением в памяти ссылки, названия

---
+++ parser selenium/skin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parser_selenium
+{
+    internal class skin
+    {
+        string Id;
+        string Name;
+        string Type;
+
+    }
+}
